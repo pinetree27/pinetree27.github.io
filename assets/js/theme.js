b5135d786fd01757fbade79a9ae150c96d56dde4
pinetree27 <164206346+pinetree27@users.noreply.github.com>
2024-12-08 20:43:04 +0100

    Update theme.js

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -5,7 +5,7 @@ let toggleThemeSetting = () => {
   let themeSetting = determineThemeSetting();
   if (themeSetting == "system") {
     setThemeSetting("dark");
-  } else if (themeSetting == "dark") {
+  } else if (themeSetting == "light") {
     setThemeSetting("dark");
   } else {
     setThemeSetting("system");
